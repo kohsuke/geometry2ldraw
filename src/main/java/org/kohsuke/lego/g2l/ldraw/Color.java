c45a451b0341be1138e971b5f3e125350fdc4ec7
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-04-12 17:28:12 -0700

    adding more primary colors

--- src/main/java/org/kohsuke/lego/g2l/ldraw/Color.java
+++ src/main/java/org/kohsuke/lego/g2l/ldraw/Color.java
@@ -7,11 +7,22 @@ package org.kohsuke.lego.g2l.ldraw;
  */
 // see http://www.ldraw.org/article/547
 public enum Color {
-    RED(4), WHITE(15);
+    BLACK       ( 0,"05131D"),
+    BLUE        ( 1,"0055BF"),
+    GREEN       ( 2,"257A3E"),
+    RED         ( 4,"C91A09"),
+    BROWN       ( 6,"583927"),
+    LIGHT_GRAY  ( 7,"9BA19D"),
+    DARK_GRAY   ( 8,"6D6E5C"),
+    LIGHT_BLUE  ( 9,"B4D2E3"),
+    WHITE       (15,"FFFFFF")
+    ;
 
     public final int id;
+    public final int c;
 
-    Color(int id) {
+    Color(int id, String code) {
         this.id = id;
+        this.c = Integer.parseInt(code, 16);
     }
 }
